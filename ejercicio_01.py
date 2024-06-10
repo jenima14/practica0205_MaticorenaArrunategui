263487007d3c549eb914dce7960bead99f00ae00
jenima14 <jmarticarr2@educacion.navarra.es>
2024-06-10 07:36:22 +0200

    ejercicio_01

---
+++ ejercicio_01.py
@@ -0,0 +1,11 @@
+def Saludo(nombre):
+    '''Escribir una función a la que se le pase una cadena <nombre> y
+       muestre por pantalla el saludo ¡hola <nombre>!.
+        Parámetro:
+                Una cadena (str)
+        Muestra en pantalla:
+                ¡hola <nombre>!  '''
+   
+    print("¡hola " + nombre + "!")
+
+Saludo("Jenifer")
